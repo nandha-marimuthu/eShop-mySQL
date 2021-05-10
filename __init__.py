77fba51ca27205fec4ceea91906a94e3c10af747
Nandhabalan Marimuthu <67896802+nandha-marimuthu@users.noreply.github.com>
2021-05-10 18:37:11 +0530

    Item add Function

--- __init__.py
+++ __init__.py
@@ -1,15 +1,2 @@
 #electronics shop with mysql
-def eShop():
-  i = int(input("1 For Order\n2 For Cancel\n3 For Admin\nEnter: "))
-  if i==1:
-    from login import login
-    login()
-  elif i==2:
-    from cancel import cancel
-    cancel()
-  elif i==3:
-    from login import admin
-    admin()
-  else:
-    print('Invalid code')
-eShop()
+from eShop import eShop
---
+++ eShop.py
@@ -0,0 +1,14 @@
+def eShop():
+  i = int(input("1 For Order\n2 For Cancel\n3 For Admin\nEnter: "))
+  if i==1:
+    from login import login
+    login()
+  elif i==2:
+    from cancel import cancel
+    cancel()
+  elif i==3:
+    from login import admin
+    admin()
+  else:
+    print('Invalid code')
+eShop()
